75629d137e3772a2889deff84d846915521805c3
Bohdan Korinnyi <bohdan.korinnyi@gmail.com>
2020-06-25 17:27:06 +0300

    init project

---
+++ src/main/java/com/alpha/emotessor/EmotessorApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.emotessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alpha/emotessor/EmotessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.emotessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
