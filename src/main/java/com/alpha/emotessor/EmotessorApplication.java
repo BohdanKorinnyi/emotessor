ca16ea4e5dec7bac0c05305ae8aba1c9247df347
Bohdan Korinnyi <bohdan.korinnyi@gmail.com>
2020-07-01 23:18:55 +0300

    add files to ignore

--- src/main/java/com/alpha/emotessor/EmotessorApplication.java
+++ src/main/java/com/alpha/emotessor/EmotessorApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmotessorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmotessorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmotessorApplication.class, args);
+    }
 
 }
